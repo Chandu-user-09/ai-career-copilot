6eef4c0ed19d3009be68b92dfc1c2f9f27ee8ee8
Chandra Prakash <user.chandraprakash@gmail.com>
2026-06-20 20:54:20 +0530

    Prepare project for deployment

--- careercopilot/settings.py
+++ careercopilot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ln*%u9)$x#788kc6&^5wjaa2b(&fen640ue25smo)2mp!a9a7o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
