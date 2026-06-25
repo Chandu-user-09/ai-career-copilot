d5302e7fb1be86b5e0da554a44f9bd173cc1075d
Chandra Prakash <user.chandraprakash@gmail.com>
2026-06-25 21:55:44 +0530

    Remove unused TensorFlow packages

--- careercopilot/settings.py
+++ careercopilot/settings.py
@@ -126,6 +126,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
